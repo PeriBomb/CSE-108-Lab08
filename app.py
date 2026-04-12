e415a85739216c86f39598f6755bc4e451f0f4bb
PeriBomb <25spaulding@gmail.com>
2026-04-11 20:00:24 -0700

    admin view

--- app.py
+++ app.py
@@ -2,21 +2,65 @@ from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from models import User, Course, Enrollment
 from extensions import db
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_login import LoginManager, login_required, current_user, login_user, logout_user
+from flask import Flask, render_template, redirect, url_for, request, flash
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+app.config["SECRET_KEY"] = "dev-secret-key"
+
+admin = Admin(app, name="Lab-08 Admin View")
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Course, db.session))
+admin.add_view(ModelView(Enrollment, db.session))
+
+
 @app.route("/")
 def index():
     return redirect(url_for("login"))
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.check_password(password):
+            login_user(user)
+            if user.role == "admin":
+                return redirect("/admin")
+            elif user.role == "teacher":
+                return redirect(url_for("teacher_dashboard"))
+            else:
+                return redirect(url_for("student_dashboard"))
+        else:
+            flash("Invalid username or password")
+
     return render_template("login.html")
 
+@app.route("/student/dashboard")
+@login_required
+def student_dashboard():
+    return render_template("student_dashboard.html")
+
+@app.route("/teacher/dashboard")
+@login_required
+def teacher_dashboard():
+    return render_template("teacher_dashboard.html")
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 if __name__ == "__main__":
-    # with app.app_context():
-    #     db.create_all()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from extensions import db
+from flask_login import UserMixin
 
-class User(db.Model):
+
+
+class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
