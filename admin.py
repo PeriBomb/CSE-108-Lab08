ccea05b3e46ddd10b9852d7be245600b0053dd23
PeriBomb <25spaulding@gmail.com>
2026-04-12 14:11:45 -0700

    Added all user accounts by changing values in admin.py, simple dashboards
    All passwords are password, all usernames/emails are visible in data.db, but are the first letter of the first name + last name
    Ex: Ralph Jenkins -> rjenkins
    Basic student and teacher dashboards that welcome the user by name and differentiate based on role.

--- admin.py
+++ admin.py
@@ -4,11 +4,11 @@ from models import User
 
 with app.app_context():
     admin = User(
-        username="admin",
-        email="admin@school.com",
-        first_name="John",
-        last_name="VSCode",
-        role="admin"
+        username="ywenchen",
+        email="ywenchen@school.com",
+        first_name="Yi",
+        last_name="Wen Chen",
+        role="student"
     )
     admin.set_password("password")
     db.session.add(admin)
